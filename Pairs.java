07be3f2161c6ba09b52cd43dd6d95e426dd17083
Tanushree Vedula <43469249+TanushreeVedula14@users.noreply.github.com>
2018-10-04 16:42:01 +0530

    Pairs

---
+++ Pairs.java
@@ -0,0 +1,46 @@
+package com.m_3.pairs;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Pairs 
+{
+	int count;
+	int size;
+	Scanner sc = new Scanner(System.in);
+	public void pair()
+	{
+		System.out.println("Enter array size:");
+		size = sc.nextInt() ;
+		
+		int[] A = new int[size];
+		for(int i=0;i<size;i++)
+		{
+			System.out.println("Enter "+i+" element:");
+			A[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(A);
+		System.out.println("Sorted order is: ");
+		
+		for(int j=0;j<A.length;j++)
+		{
+		
+			System.out.print(" "+A[j]);
+			
+		}
+		for(int i=0;i<(A.length-1);i++)
+		{
+		
+			if(A[i] == A[i+1])
+			{
+				count++;			
+			}
+		
+		}
+		System.out.println();
+		System.out.println("No of equal pairs: "+count);
+	}
+}
+
+
