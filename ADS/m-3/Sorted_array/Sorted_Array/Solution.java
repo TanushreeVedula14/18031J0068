4852f000502e9590727b4c2a529890dc713e23d4
TanushreeVedula14 <tanushree.vedula14@gmail.com>
2018-10-05 14:09:49 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS/m-3/Sorted_array/Sorted_Array/Solution.java
+++ ADS/m-3/Sorted_array/Sorted_Array/Solution.java
@@ -1,68 +1,92 @@
-import java.util.Arrays;
-import java.util.Scanner;
+package com.m_3.a2;
 
-class MergeArray 
-{ 
-    int mergeArray(int A[], int B[], int m, int n) 
-    { 
-    	int C[] = new int[m+n];
-    	
-    	Arrays.sort(A);
-    	Arrays.sort(B);
-    	
-    	int i = 0;
-    	int j = 0;
-    	int k = 0;
-      
-    	while (i < m && j < n) 
-    	{ 
-    		C[k++] = A[i++]; 
-	        C[k++] = B[j++]; 
-	    } 
-    	while (i < m) 
-            C[k++] = A[i++]; 
-    	
-    	while (j < n) 
-            C[k++] = B[j++]; 
-    	
-    	for(k=0;k<(m+n);k++)
-    	{
-    		if(k<(m+n-1))
-    		System.out.print(C[k]+",");
-    		else
-    			System.out.println(C[k]);
-    	}
-    	
-      return 0;
-             
-    } 
-} 
-public class Solution 
-{
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		//System.out.println("Enter A[] size:");
-		int m =sc.nextInt();
-		int A[] = new int[m];
-		//System.out.println("Enter A[] elements:");
-		for(int i=0;i<m;i++)
-		{
-			A[i] = sc.nextInt();
+	import java.io.*;
+
+	public class Solution {
+
+		public static void main(String[] args)throws Exception {
+			// TODO Auto-generated method stub
+	     int[] a=null;
+	     int[] b=null;
+	     int n,m;
+	    
+		 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	     //System.out.println("enter number of elements in first array");
+	    String s1=br.readLine();
+	     //System.out.println("enter number of elements in second array");
+	     String s2=br.readLine();
+	     n=Integer.parseInt(s1);
+	     m=Integer.parseInt(s2);
+	    // System.out.println("enter array elements");
+	String w1[]=null;
+	String w2[]=null;
+
+	     a=new int[n];
+	     b=new int[m];
+	String s=br.readLine();
+	//System.out.println(s);
+	String s3=br.readLine();
+	//System.out.println(s3);
+	w1=s.split("[,.]");
+	w2=s3.split("[,.]");
+
+
+
+	     for(int i=0;i<n;i++)
+	     {
+	    	 //System.out.println("a["+i+"]");
+	    	 
+	    	 a[i]=Integer.parseInt(w1[i]);
+	    	 
+	     }
+	     for(int j=0;j<m;j++)
+	     {
+	    	// System.out.println("b["+j+"]");
+	    	 //String s=sc.next();
+	    	 b[j]=Integer.parseInt(w2[j]);
+	     }
+	     int[] c;
+	     int i=0,j=0,k=0;
+	     c=new int[m+n];
+	     while(i<a.length&&j<b.length)
+	     {
+		   if(a[i]>b[j])
+		   {
+			   c[k]=b[j];
+			   k++;
+			   j++;
+		   }
+		   else
+		   {
+			   c[k]=a[i];
+			   k++;
+			   i++;
+		   }
+	      }
+	     while(i<a.length)
+	     {
+	    	 c[k]=a[i];
+	    	 k++;
+	    	 i++;
+	     }
+	     while(j<b.length)
+	     {
+	    	 c[k]=b[j];
+	    	 k++;
+	    	 j++;
+	     }
+	     //System.out.println("after sorting");
+	     for(k=0;k<c.length;k++)
+	     {
+	         if(k<c.length-1)
+	    	 System.out.print(c[k]+",");
+	         else
+	       System.out.print(c[k]);
+	     }
+
 		}
-		
-		//System.out.println("Enter B[] size:");
-		int n = sc.nextInt();
-		int B[] = new int[n];
-		//System.out.println("Enter B[] elements:");
-		for(int i=0;i<n;i++)
-		{
-			B[i] = sc.nextInt();
-		}
-		
-		MergeArray ma = new MergeArray();
-		ma.mergeArray(A,B, m, n);
+
 	}
 
-}
+
+
