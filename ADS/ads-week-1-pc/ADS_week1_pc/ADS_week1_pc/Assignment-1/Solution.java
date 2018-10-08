8464ff6f929721dd54b260018aa4a98b7184e7eb
TanushreeVedula14 <tanushree.vedula14@gmail.com>
2018-10-08 17:36:27 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- ADS/ads-week-1-pc/ADS_week1_pc/ADS_week1_pc/Assignment-1/Solution.java
+++ ADS/ads-week-1-pc/ADS_week1_pc/ADS_week1_pc/Assignment-1/Solution.java
@@ -1,3 +1,5 @@
+package com.w1.pc;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,94 +11,64 @@ class AddLinkedList
 	class Node
 	{
 		Node next;
-		int data;
+		long data;
+		int size;
 		
-		Node(int d)
+		Node(long d)
 		{
 			data = d;
 			next = null;
-		}
-	}
-	
-	public void addNode(int new_data)
-	{
-		Node new_node = new Node(new_data);
-		new_node.next = head;
-		head = new_node;
-	}
-	
-	@SuppressWarnings({ "unused", "null" })
-	Node addLargenumbers(Node n1, Node n2)
-	{
-		
-		int carry = 0;
-		int sum = 0;
-		int iteration = 0;
-		
-		Node new_node1 = null;
-		Node carryNode = null;
-		
-		while(n1 != null && n2 != null)
-		{
-			iteration++;
-			if(n1 != null)
-			{
-				sum = sum + n1.data;
-				n1 = n1.next;
-			}
-			if(n2 != null)
-			{
-				sum = sum + n2.data;
-				n2 = n2.next;
-			}
-			
+			size = 0;
 		}
 		
-		new_node1.data = sum;
-		new_node1.next = new_node1;
-		
-		return new_node1;
 		
 	}
+	
+	Node numberToDigits(String number)
+	{
+		
+			long m = Integer.parseInt(number);
+			Node new_node = new Node(m);
+			new_node.next = head;
+			head = new_node;
+		
+		return head;
+	}
+	
 	public void display()
 	{
 		Node n = head;
 		while(n != null)
 		{
-			System.out.print(n.data+" ");
+			System.out.println(n.data);
 			n = n.next;
 		}
+		
 	}
 
 }
 
-class Solution 
+public class Solution 
 {
 
 	public static void main(String[] args) throws IOException 
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//System.out.println("Enter string:");
-		String line = br.readLine();
-		int m = Integer.parseInt(line);
-		
 		AddLinkedList all = new AddLinkedList();
-		all.addNode(m);
+		
+		System.out.println("Enter string:");
+		String line = br.readLine();
+		
+		System.out.println("Enter string1:");
+		String line1 = br.readLine();
+		
+		
+		
+		all.numberToDigits(line1);
+		all.numberToDigits(line);
+		
 		all.display();
 		
-		//System.out.println("\n1st Node:");
-		all.addNode(9);
-		all.addNode(9);
-		all.addNode(9);
-		all.display();
-		
-		//System.out.println("\n2nd Node:");
-		all.addNode(1);
-		all.addNode(1);
-		all.addNode(1);
-		all.display();
-
 	}
 
 }
