dbe3bf5d1df8c69c626b4c75edfaae5b44dd7024
TanushreeVedula14 <tanushree.vedula14@gmail.com>
2018-10-12 09:17:27 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- ADS/m-5/Deque/5.1 Deque/Solution.java
+++ ADS/m-5/Deque/5.1 Deque/Solution.java
@@ -1,205 +1,188 @@
-package com.m_5.doubleQueue;
+package com.m_5.ass1;
 
 import java.util.Scanner;
-
-class DoubleQueue1 
+/**
+ * Class for deque.
+ */
+class Deque
 {
-	int A[];
-	int rear;
-	int front;
-	int size;
-	int length;
-	
-	public DoubleQueue1(int size) 
-	{
-		A = new int[size];
-		rear = -1;
-		front = -1;
-		this.size = size;
-		length = 0;
-	}
-	
-	public boolean isFull()
-	{
-		return ((front == -1 && rear == size-1) || front == rear+1);
-	}
-	
-	public boolean isEmpty()
-	{
-		return (front == -1 && rear == -1);
-	}
-	
-	public int getSize()
-	{
-		return length;
-	}
-	
-	public void insertFront(int ele) 
-	{
-		/*if(isFull())
-		{
-			System.err.println("Front overflow.");
-		}
-		else*/
-		{
-			if(front == -1)
-			{
-				front = 0;
-				rear = 0;
-			}
-			else if(front == 0)
-			{
-				front = size-1;
-			}
-			else
-			{
-				front = front-1;
-			}
-		
-			A[front] = ele;
-			//System.out.print("["+ele+"]"+" ");
-			length++;
-		}
-	}
-	
-	public void insertRear(int ele)
-	{
-		/*if(isFull())
-		{
-			System.err.println("Rear overflow.");
-		}*/
-		if(front == -1)
-			{
-				front = 0;
-				rear = 0;
-			}
-			else if(rear == size-1)
-			{
-				rear = 0;
-			}
-			else
-			{
-				rear = rear+1;
-			}
-			A[rear] = ele;
-			//System.out.print("["+ele+"]"+" ");
-			length++;
-			
-		
-	}
-	
-	public void deleteFront()
-	{
-		/*if(isEmpty())
-		{
-			System.err.println("No element to delete.");
-		}*/
-		if(front == rear)
-		{
-				front = -1;
-				rear = -1;
-			}
-			else if(front == size-1)
-			{
-				front = 0;
-			}
-			else
-			{
-				front = front+1;
-			}
-			//B = new int[ele];
-			length--;
-		}
-	
-	
-	public void deleteRear()
-	{
-		/*if(isEmpty())
-		{
-			System.err.println("Rear empty.");
-		}*/
-		if(front == rear)
-		{
-				front = -1;
-				rear = -1;
-			}
-			else if(rear == 0)
-			{
-				rear = size-1;
-			}
-			else
-			{
-				rear = rear-1;
-			}
-			length--;
-		}
-	
-	
-	public int getFront()
-	{
-		if(isEmpty())
-		{
-			//System.out.println("Front empty.");
-			return -1;
-		}
-		return A[front];
-	}
-	
-	public int getRear()
-	{
-		if(isEmpty())
-		{
-			//System.out.println("Rear empty.");
-			return -1;
-		}
-		return A[rear];
-	}
-	public void display()
+    
+    int elements;
+    Node first,last;
+    
+    class Node
     {
-        //System.out.print("\nQueue: ");
-       
-        for (int i = 0; i < A.length; i++)
-            System.out.print(A[i]+" ");
-        System.out.println();
-    }    
+        String data;
+         Node next;
+        
+        Node(String val,Node link)
+        {
+            this.data = val;
+            this.next = link;
+        }
+    }
+    
+    Deque()
+    {
+        elements = 0;
+        first = null;
+        last = null;
+    }
+   
+    public void pushLeft(String value)
+    {
+        if(first==null) 
+        {
+            first=new Node(value,null);
+            last=first;
+        } 
+        else
+        {
+            Node newnode=new Node(value,first);
+             first=newnode;
+        }
+
+        elements++;
+    }
+    
+    public void pushRight(String value) 
+    {
+        if (last==null)
+        {
+            last=new Node(value,null);
+            
+            first=last;
+        } 
+        else 
+        {
+            Node newnode=new Node(value, null);
+            
+            last.next=newnode;
+            last=newnode;
+        }
+        elements++;
+    }
+    /**
+     * popleft.
+     */
+    void popLeft() 
+    {
+        if(first!=null) 
+        {
+            Node popped=first;
+            first=first.next;
+            popped.data=null;
+            popped.next=null;
+            elements--;
+        }
+    }
+    /**
+     * popright.
+     */
+    void popRight()
+    {
+        if (last!=null) 
+        {
+            Node temp=null;
+            Node popped=last;
+            Node element=first;
+            while(element!=last)
+            {
+                temp=element;
+                element=element.next;
+            }
+            last = temp;
+            last.next=null;
+            popped.data=null;
+            popped.next=null;
+            elements--;
+        }
+    }
+    
+    int size() 
+    {
+        return elements;
+    }
+    
+    boolean isEmpty()
+    {
+        return first==null;
+    }
+    
+    public String print() 
+    {
+        if (elements != 0) 
+        {
+            String str = "";
+            Node temp = first;
+            while (temp != null) 
+            {
+                str += temp.data + ", ";
+                temp = temp.next;
+            }
+            return "[" +str.substring(0,str.length()-2)+ "]";
+        }
+        return "[]";
+
+    }
+
 }
 
 class Solution
 {
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		//System.out.println("Enter Queue Size:");
-		int n = sc.nextInt();
-		
-		DoubleQueue1 dq = new DoubleQueue1(n);
-		int ch=0;
-		
-		do {
-			//System.out.println("\nQueue Operations:");
-			//System.out.println("\n1.Insert Front.\n2.Insert Rear.\n3.Delete Front.\n4.Delete Rear.\n5.Size.\n6.Display.");
-			ch = sc.nextInt();
-			switch(ch)
-			{
-				case 1: //System.out.println("Enter element in front:");
-						int ele = sc.nextInt();
-						dq.insertFront(ele);
-						break;
-				case 2: //System.out.println("Enter element in rear:");
-						int ele1 = sc.nextInt();
-						dq.insertRear(ele1);
-						break;
-				case 3: dq.deleteFront();
-						break;
-				case 4: dq.deleteRear();
-						break;
-				case 5: System.out.println(dq.getSize());
-						break;
-				case 6: dq.display();
-						break;
-			}
-		}while(ch<7);
-		sc.close();
-	}
+    
+    
+    public static void main(final String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        Deque d=new Deque();
+        for(int i=0;i<n;i++)
+        {
+            String input[] = sc.nextLine().split(" ");
+            switch (input[0]) 
+            {
+            case "pushLeft":
+                d.pushLeft(input[1]);
+                System.out.println(d.print());
+                break;
+            case "pushRight":
+                d.pushRight(input[1]);
+                System.out.println(d.print());
+                break;
+            case "popLeft":
+                if(!d.isEmpty()) 
+                {
+                    d.popLeft();
+                    System.out.println(d.print());
+                } 
+                else 
+                {
+                    System.out.println("Deck is empty");
+                }
+                break;
+            case "popRight":
+                if (!d.isEmpty())
+                {
+                    d.popRight();
+                    System.out.println(d.print());
+                } 
+                else
+                {
+                    System.out.println("Deck is empty");
+                }
+                break;
+            case "size":
+                System.out.println(d.size());
+                break;
+            case "isEmpty":
+                System.out.println(d.isEmpty());
+                break;
+            default:
+                break;
+
+            }
+        }
+    }
 }
